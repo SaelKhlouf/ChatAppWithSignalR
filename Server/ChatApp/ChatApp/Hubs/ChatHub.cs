3cfb029a56d071c237e62fbbd4d17ff38993ffe4
SaelKhlouf <sael.khlouf@gmail.com>
2021-07-19 01:27:08 +0300

    Pure JS Client + SignalR .Net Core 5

---
+++ Server/ChatApp/ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("--> Connection Opened: " + Context.ConnectionId);
+            Clients.Client(Context.ConnectionId).SendAsync("ReceiveConnID", Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendMessageAsync(string message)
+        {
+            var routeOb = JsonConvert.DeserializeObject<dynamic>(message);
+            Console.WriteLine("To: " + routeOb.To.ToString());
+            Console.WriteLine("Message Recieved on: " + Context.ConnectionId);
+            if (routeOb.To.ToString() == string.Empty)
+            {
+                Console.WriteLine("Broadcast");
+                await Clients.All.SendAsync("ReceiveMessage", message);
+            }
+            else
+            {
+                string toClient = routeOb.To;
+                Console.WriteLine("Targeted on: " + toClient);
+
+                await Clients.Client(toClient).SendAsync("ReceiveMessage", message);
+            }
+        }
+    }
+}
--- Server/ChatApp/ChatApp/Startup.cs
+++ Server/ChatApp/ChatApp/Startup.cs
@@ -1,16 +1,11 @@
+using System;
+using ChatApp.Hubs;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ChatApp
 {
@@ -32,6 +27,8 @@ namespace ChatApp
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ChatApp", Version = "v1" });
             });
+            services.AddSignalR();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,11 +45,18 @@ namespace ChatApp
 
             app.UseRouting();
 
+            app.UseCors(builder => builder
+                .WithOrigins("null")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials());
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHub<ChatHub>("/chatHub");
             });
         }
     }
